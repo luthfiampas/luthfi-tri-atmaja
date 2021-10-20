b91835fb99392636cbaa96f43de98ecf4020e6ff
luthfiampas <luthfi.personal@gmail.com>
2021-10-21 01:23:28 +0700

    more dynamic header icon

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,14 +1,14 @@
 @extends('_layouts.default')
 
 @section('content')
-<h1 class="hi">Hi, I'm Luthfi!</h1>
+<h1 class="has-icon" data-icon="&#128075;">Hi, I'm Luthfi!</h1>
 <p>Just an ordinary software engineer who always trying to improve.</p>
 <p>I've been working with <a href="https://dotnet.microsoft.com/" rel="nofollow" target="_blank">C# and Microsoft .NET</a> since 2016. I write blog posts in free time, coding at day and have a good sleep at night!</p>
 <p>Curious about how I work? Check out my..</p>
 <p>
-    <a href="/portfolio" class="link-button">Portfolio</a>
-    <a href="/articles" class="link-button">Articles</a>
-    <a href="https://github.com/luthfiampas" rel="nofollow" target="_blank" class="link-button">GitHub</a>
-    <a href="https://linkedin.com/in/luthfiampas" rel="nofollow" target="_blank" class="link-button">LinkedIn</a>
+    <a href="/portfolio" class="button">Portfolio</a>
+    <a href="/articles" class="button">Articles</a>
+    <a href="https://github.com/luthfiampas" rel="nofollow" target="_blank" class="button">GitHub</a>
+    <a href="https://linkedin.com/in/luthfiampas" rel="nofollow" target="_blank" class="button">LinkedIn</a>
 </p>
 @endsection

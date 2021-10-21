82766e0e4b0e44b59a4dd16246773148d7d662e1
luthfiampas <luthfi.personal@gmail.com>
2021-10-21 15:23:20 +0700

    change articles page description

--- source/articles.blade.php
+++ source/articles.blade.php
@@ -1,6 +1,6 @@
 ---
 title: Articles
-description: I write blog posts in my free time. This is just a collection of my thoughts and writing about programming, computer, music, sport and everything.
+description: I write blog posts in my free time. This is just a collection of my thoughts and writing about programming, computer and everything.
 ---
 
 @extends('_layouts.page')
